c4e30bf7ec63d59038fdadddbabab5c4b3f2aecb
dewzye2 <prankbooh1@gmail.com>
2022-04-28 13:05:47 -0300

    Added signature

---
+++ src/pages/assinatura.jsx
@@ -0,0 +1,9 @@
+import assinatura from './assinatura.png'
+
+function Dewzye1() {
+  return (
+  <img src={assinatura} alt='Not Found!' style={{ maxWidth: '50%', marginBottom: '00px'}}/>
+  )
+}
+
+export default Dewzye1
